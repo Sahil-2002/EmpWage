0e720a1cd219c82e0ab9b1c68c38f3d851a345d7
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-07 17:23:20 +0530

    [Sahil]Add : Employee Attendance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Random;
+//Check Employee is or present or Absent
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to Employee Wage computation ");
+    public static void main(String[] args) {
+        Random r = new Random();
+        int present=0;
+        int absent =0;
+
+        for (int i = 1; i <=30; i++) {
+            boolean attendance = r.nextBoolean();
+            if (attendance) {
+                present++;
+                System.out.println("employee is present");
+            } else {
+                absent++;
+
+                System.out.println("employee is absent ");
+            }
+
+        }
+        System.out.println("total present count is "+present);
+        System.out.println("total absent count is "+absent);
     }
 }
