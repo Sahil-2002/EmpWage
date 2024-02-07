8f85f63f60a55a4b02209169fe5760630eb8150b
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-07 18:19:25 +0530

    [Sahil]add : Employee daily wage computed

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         Random r = new Random();
         int present=0;
         int absent =0;
+        int perhour =20;
+
 
         for (int i = 1; i <=30; i++) {
             boolean attendance = r.nextBoolean();
@@ -18,7 +20,16 @@ public class Main {
             }
 
         }
+        int hour =r.nextInt(9);
+        if(hour>=8){
+            System.out.println("wage of day is "+perhour*hour);
+        } else if (hour<8) {
+            System.out.println("wage of day is "+perhour*hour);
+
+        }
+
         System.out.println("total present count is "+present);
         System.out.println("total absent count is "+absent);
+
     }
 }
