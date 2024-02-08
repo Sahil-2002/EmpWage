906484839f7067cf0185e64fd0cb52e7e06e023a
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-08 17:37:51 +0530

    [Sahil]updated part time wage

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
-        int present=0;
-        int absent =0;
-        int perhour =20;
+        int present = 0;
+        int absent = 0;
+        int perhourwage = 20;
 
 
-        for (int i = 1; i <=30; i++) {
+        for (int i = 1; i <= 30; i++) {
             boolean attendance = r.nextBoolean();
             if (attendance) {
                 present++;
@@ -20,16 +20,36 @@ public class Main {
             }
 
         }
-        int hour =r.nextInt(9);
-        if(hour>=8){
-            System.out.println("wage of day is "+perhour*hour);
-        } else if (hour<8) {
-            System.out.println("wage of day is "+perhour*hour);
+        int hour = r.nextInt(9);
+        if (hour >= 8) {
+            System.out.println("wage of day is " + perhourwage * hour);
+        } else if (hour < 8) {
+            System.out.println("wage of day is " + perhourwage * hour);
 
         }
 
-        System.out.println("total present count is "+present);
-        System.out.println("total absent count is "+absent);
+        System.out.println("total present count is " + present);
+        System.out.println("total absent count is " + absent);
+
+
+        // parttime employee wage
+        int parttime_hour = 8;
+
+            System.out.println("part time wage is "+parttime_hour*perhourwage);
+
 
     }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
